8b6d06ba311134707a146adf187486df0a0a0af6
ai-uni <105314864+ai-uni@users.noreply.github.com>
2022-05-11 02:03:42 +0530

    Update Exp05 BFS(Best first).py

--- Exp05 BFS(Best first).py
+++ Exp05 BFS(Best first).py
@@ -1,42 +1,41 @@
 from queue import PriorityQueue
-v = 14
-graph = [[] for i in range(v)]
+import matplotlib.pyplot as plt
+import networkx as nx
 
+# for implementing BFS | returns path having lowest cost
 def best_first_search(source, target, n):
-	visited = [False] * n
-	visited = True
-	pq = PriorityQueue()
-	pq.put((0, source))
-	while pq.empty() == False:
-		u = pq.get()[1]
-		print(u, end=" ")
-		if u == target:
-			break
+    visited = [0] * n
+    visited[source] = True
+    pq = PriorityQueue()
+    pq.put((0, source))
+    while pq.empty() == False:
+        u = pq.get()[1]
+        print(u, end=" ") # the path having lowest cost
+        if u == target:
+            break
 
-		for v, c in graph[u]:
-			if visited[v] == False:
-				visited[v] = True
-				pq.put((c, v))
-	print()
+        for v, c in graph[u]:
+            if visited[v] == False:
+                visited[v] = True
+                pq.put((c, v))
+    print()
 
+# for adding edges to graph
 def addedge(x, y, cost):
-	graph[x].append((y, cost))
-	graph[y].append((x, cost))
+    graph[x].append((y, cost))
+    graph[y].append((x, cost))
 
-addedge(0, 1, 3)
-addedge(0, 2, 6)
-addedge(0, 3, 5)
-addedge(1, 4, 9)
-addedge(1, 5, 8)
-addedge(2, 6, 12)
-addedge(2, 7, 14)
-addedge(3, 8, 7)
-addedge(8, 9, 5)
-addedge(8, 10, 6)
-addedge(9, 11, 1)
-addedge(9, 12, 10)
-addedge(9, 13, 2)
+G = nx.Graph()
+v = int(input("Enter the number of nodes: "))
+graph = [[] for i in range(v)] # undirected Graph
+e = int(input("Enter the number of edges: "))
+print("Enter the edges along with their weights:")
+for i in range(e):
+    x, y, z = list(map(int, input().split()))
+    addedge(x, y, z)
+    G.add_edge(x, y, weight = z)
 
-source = 0
-target = 9
+source = int(input("Enter the Source Node: "))
+target = int(input("Enter the Target/Destination Node: "))
+print("\nPath: ", end = "")
 best_first_search(source, target, v)
